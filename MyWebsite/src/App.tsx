532c240b9013e9ffd550493b27a4c4ba7662cb28
Jinish Patel <jinish5597@gmail.com>
2023-05-24 09:58:28 -0400

    installation

---
+++ MyWebsite/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+  return(
+    <div><span>hello jinish</span></div>
+  )
+}
+
+export default App
