b4ce763d34cd0da1f6ba629ef29b93040968bbb5
Jinish Patel <jinish5597@gmail.com>
2023-06-02 09:42:15 -0400

    ..

--- MyWebsite/src/Components/Footer.tsx
+++ MyWebsite/src/Components/Footer.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import './footer.css'
-const 
-Footer = () => {
+import React from "react";
+import "./footer.css";
+import { FaHome, FaMobile, FaVoicemail } from "react-icons/fa";
+const Footer = () => {
   return (
-    <div>
-        Footer
+    <div className="Footer">
+      <div className="Footer-container">
+        <div className="left">
+          <div className="location">
+            <FaHome />
+          </div>
+          <div className="mobile">
+            <FaMobile />
+          </div>
+          <div className="email">
+            <FaVoicemail />
+          </div>
+        </div>
+        <div className="right"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default 
-Footer
+export default Footer;
--- MyWebsite/src/routes/home.tsx
+++ MyWebsite/src/routes/home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import HeroImg from "../Components/HeroImg";
+import Footer from "../Components/Footer";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroImg />
+      <Footer/>
     </div>
   );
 };
