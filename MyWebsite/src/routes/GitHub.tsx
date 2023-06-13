1533f1d18b4a04aaf71942b9425dfe0e2fc1a0d2
Jinish Patel <jinish5597@gmail.com>
2023-06-13 00:06:23 -0400

    second

--- MyWebsite/src/routes/GitHub.tsx
+++ MyWebsite/src/routes/GitHub.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import Navbar from '../Components/Navbar';
+// import React from 'react'
+import Navbar from "../Components/Navbar";
 
 const GitHub = () => {
-  return (
-      <Navbar />
-  )
-}
+  return <Navbar />;
+};
 
-export default GitHub
+export default GitHub;
